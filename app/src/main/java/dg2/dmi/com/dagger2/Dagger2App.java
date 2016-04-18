f09795c1b52f5aa16689246a0583d9ac623b4064
Polen Chheang <pc@goldengekko.com>
2016-04-18 14:06:07 +0700

    Attemp pullrequest 3

--- app/src/main/java/dg2/dmi/com/dagger2/Dagger2App.java
+++ app/src/main/java/dg2/dmi/com/dagger2/Dagger2App.java
@@ -6,15 +6,10 @@ import android.app.Application;
  * Created by gguser on 1/29/16.
  */
 public class Dagger2App extends Application {
-//    private AppComponent appComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
-//        appComponent = DaggerAppComponent.builder().appModule(new AppModule(this)).build();
     }
 
-//    public AppComponent getAppComponent() {
-//        return appComponent;
-//    }
 }
