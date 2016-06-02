c7d453ae2923dd9169db597c8df387021ae46149
Polen Chheang <pchheang@dminc.com>
2016-06-02 17:15:59 +0700

    Add refresh Asset

--- app/src/main/java/dg2/dmi/com/dagger2/activity/MainActivity.java
+++ app/src/main/java/dg2/dmi/com/dagger2/activity/MainActivity.java
@@ -4,21 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import javax.inject.Inject;
 
+import butterknife.OnClick;
 import dg2.dmi.com.dagger2.R;
 import dg2.dmi.com.dagger2.product.dagger.ProductListInjectable;
 import dg2.dmi.com.dagger2.product.presenter.ProductListPresenter;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @OnClick(R.id.fab)
-//    void onRefresh() {
-//        mPresenter.getProduct();
-//    }
+    @OnClick(R.id.fab)
+    void onRefresh() {
+        mPresenter.getProduct();
+    }
 
     @Inject
     ProductListPresenter mPresenter;
@@ -39,28 +38,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         mPresenter.onCreate();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
